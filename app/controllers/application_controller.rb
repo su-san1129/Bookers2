d693137e9c6e234f7ec064547844c80cf92a91bf
su-san1129 <deviationvalue1129@icloud.com>
2019-04-15 15:47:56 +0900

    [修正]Bookers2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,8 +8,8 @@ before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
-    devise_parameter_sanitizer.permit(:sign_in, keys: [:username])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :email])
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:username, :email])
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,19 +4,12 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  def email_required?
-  	false
-  end
-  def email_changed?
-  	false
-  end
-
   has_many :books, dependent: :destroy
 
   attachment :profile_image
 
 
-  validates :username, presence: true, length: { maximum: 50 }
+  validates :username, presence: true, length: { minimum: 2, maximum: 50 }
   validates :introduction, length: { maximum: 50 }
 
 
